428e9e611fda6c68511e4ed75de7542a134942a6
Greenk25 <kalkidandegif@gmail.com>
2025-02-23 11:36:49 -0800

    5

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Library
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    # Logic to add a book
+    pass
 
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Logic to edit a book
+    pass
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Logic to delete a book
+    pass
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
