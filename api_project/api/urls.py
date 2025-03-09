e6c0b6b40c3cea00ba8eda019f7b96af156e7e4b
Greenk25 <kalkidandegif@gmail.com>
2025-03-08 22:28:29 -0800

    bookset

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,3 @@
-from django.urls import path
-from .views import BookList
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BookList, BookViewSet
@@ -10,4 +8,5 @@ urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),I
     path('', include(router.urls)),
 ]
+
 
