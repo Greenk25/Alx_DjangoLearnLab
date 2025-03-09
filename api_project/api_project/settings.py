8e6431a775ad6294f20eab69972f9ebb55608828
Greenk25 <kalkidandegif@gmail.com>
2025-03-08 19:16:02 -0800

    apiProject

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -12,14 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-INSTALLED_APPS = [
-    ...,
-    'rest_framework',
-]
-INSTALLED_APPS = [
-    ...,
-    'api',
-]
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1r*46j(sm(0x5)gf_ifka*jm0raqlupi*u5#cg2acccl4ue*$%'
+SECRET_KEY = 'django-insecure-)e_*e@3xx#&$kf8k=w@(t5bd)ory1gg5u995fepzk)ixlcy5=k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -46,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
