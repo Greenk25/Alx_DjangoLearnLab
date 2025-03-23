4cc5265311261649e17476a3958ed5d39f5105f6
Greenk25 <kalkidandegif@gmail.com>
2025-03-23 09:39:58 -0700

    Adding Comment Functionality to Blog Posts

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,11 @@
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django import forms
 from .models import Post
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')  # Link to Post model
     author = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to User model
@@ -15,6 +15,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.author} on {self.post.title}"
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
